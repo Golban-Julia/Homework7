1784ebfab7d2ca804ffbddd6c0a2e67207242b96
Golban-Julia <golden95juli@gmail.com>
2022-08-02 23:06:47 +0300

    decided task 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// 1. Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n. Для решения задачи необходим использовать рекурсию.
+
+function sumTo(n) {
+    if (n === 1) {
+        return n;
+    }
+
+ return n + sumTo(n - 1);
+}
+
+console.log(sumTo(4)); // 10
+console.log(sumTo(3)); // 6
