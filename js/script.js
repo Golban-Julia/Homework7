762e983d14c0de565949a92573b7aab03a7e2902
Golban-Julia <golden95juli@gmail.com>
2022-08-03 23:28:02 +0300

    decided task 2

--- js/script.js
+++ js/script.js
@@ -2,13 +2,39 @@
 
 // 1. Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n. Для решения задачи необходим использовать рекурсию.
 
-function sumTo(n) {
-    if (n === 1) {
-        return n;
-    }
+// 1. Рекурсия - n - 1 
+// 2. Базис рекурсии - условие выхода из рекурсии 
+// 3. Суммируем
 
- return n + sumTo(n - 1);
+// function sumTo(n) {
+//     if (n === 1) {
+//         return n;
+//     }
+
+//  return n + sumTo(n - 1);
+// }
+
+// const sumToShirt = (n) => (n === 1 ? n : n + sumTo(n - 1));
+
+// console.log(sumTo(4)); // 10
+// console.log(sumToShirt(3)); // 6
+
+// 2. Написать функцию findShort, которая будет находит самое короткое слово в предложении.
+//  В случае если длина двух слов совпадает - вернуть первое найденое короткое слово.Пример работы функции:
+
+// 1. Преобразовать строку в массив;
+// 2. Отсортировать эллементы массива по длине от меньшего к большему;
+// 3. Вернуть первый эллемент массива.
+
+function findShort(string) {
+    let array = string.split(" ").sort((a, b) => (a.length - b.length));
+    return array[0];
 }
 
-console.log(sumTo(4)); // 10
-console.log(sumTo(3)); // 6
+const sentance = "Lorem ipsum dolor sit amet";
+
+
+console.log(findShort(sentance)); // sit
+console.log(findShort("Hello world")); // Hello
+console.log(findShort("Hi")); // Hi
+console.log(findShort("She is David's sister")); // is
